6f978507dfdfab6cc83a452c90c1b0b4d882b0cb
Steven <albertstevenmf@hotmail.com>
2024-08-27 00:18:10 -0400

    Entrypoints updated

--- config/story/start_node.sh
+++ config/story/start_node.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 echo "Initializing Story's beacon node"
-/story-node/story/story init --network iliad --home /story-node/story/ --force
+/story-node/.story/story/story init --network iliad --home /story-node/.story/story/ --force
 
 
 echo "Starting Story's beacon node"
-/story-node/story/story run --home="/story-node/story/"
+/story-node/.story/story/story run --home="/story-node/.story/story/"
