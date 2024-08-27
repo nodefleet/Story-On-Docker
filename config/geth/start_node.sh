9e109c4b08954cbc2d85ab7d10edea0d39d116b7
Steven <albertstevenmf@hotmail.com>
2024-08-27 00:35:37 -0400

    Geth entrypoint updated

--- config/geth/start_node.sh
+++ config/geth/start_node.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 echo "Starting geth binary for Story Testnet"
-/story-node/.story/geth/geth --iliad --syncmode full
+/story-node/.story/geth/geth --iliad --syncmode full --config /story-node/.story/geth/config/geth.toml
